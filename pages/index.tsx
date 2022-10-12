284e2d4eafb2e02bbcd524a31e09f06d74dfcac4
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2022-10-12 16:10:22 +0300

    Project done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,45 @@
-import Layout               from '../components/Layout'
-import Card                 from "../components/Index/Card";
-import {GetServerSideProps} from "next";
-import House                from "../interfaces/house";
-import {getHouses}          from "../services/houses";
+import Layout      from '../components/Layout'
+import Card        from "../components/Index/Card";
+import House       from "../interfaces/house";
+import useSWR      from "swr";
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const houses: House[] = await getHouses();
-  return {props: {houses}}
-}
+// @ts-ignore
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 type IndexPageProps = {
   houses: House[];
 }
 
-const IndexPage = ({houses}: IndexPageProps) => (
-  <Layout title="Home | Homi Demo">
-    <div className={"m-3 p-4"}>
-      <div className={"grid grid-cols-4 gap-5"}>
-        {houses.map((house: House) => (
-          <Card key={house.listing_id} house={house}/>
-        ))}
-      </div>
-    </div>
-  </Layout>
-)
+const IndexPage = ({houses}: IndexPageProps) => {
+  const {data, error} = useSWR('/api/houses', fetcher);
+
+  if (error) {
+    return (
+      <Layout title={"Houses Not Found | Homi Demo"}>
+        <h1>{error.log}</h1>
+      </Layout>
+    )
+  }
+
+  if (data) {
+    const houses: House[] = data;
+
+    return (
+      <Layout title="Home | Homi Demo">
+        <div className={"m-3 p-4"}>
+          <div className={"grid grid-cols-4 gap-5"}>
+            {houses.map((house: House) => (
+              <Card key={house.listing_id} house={house}/>
+            ))}
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+
+  return (
+    <h1>Hi</h1>
+  )
+}
 
 export default IndexPage
--- services/houses.ts
+++ services/houses.ts
@@ -4,7 +4,8 @@ import axios from "axios";
 const url = "http://localhost:3000/api/houses";
 
 const getHouses = async (): Promise<House[]> => {
-  const {data} = await axios.get(url);
+  // const {data} = await axios.get(url);
+  const {data} = await fetch('/api/houses');
   return data;
 }
 
