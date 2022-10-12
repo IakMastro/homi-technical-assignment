75b80146e874398cde618b77866ca5149257dbe6
Iakovos Mastrogiannopoulos <iakmastro@outlook.com.gr>
2022-10-12 16:12:16 +0300

    Project done

--- services/houses.ts
+++ services/houses.ts
@@ -4,8 +4,7 @@ import axios from "axios";
 const url = "http://localhost:3000/api/houses";
 
 const getHouses = async (): Promise<House[]> => {
-  // const {data} = await axios.get(url);
-  const {data} = await fetch('/api/houses');
+  const {data} = await axios.get(url);
   return data;
 }
 
